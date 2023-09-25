749a33044171aaa6ed7ff32ce94b7854b2915880
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2023-09-25 23:37:43 +0300

    navigate

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,39 +1,47 @@
-import React, { createContext } from 'react'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import {auth} from '../auth/firebase';
-export const AuthContext=createContext()
+import React, { createContext } from "react";
+import {
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../auth/firebase";
+import { useNavigate } from "react-router";
+export const AuthContext = createContext();
 
 //* with custom hook
 /* export const useAuthContext = () =>{
     return useContext(AuthContext)
 } */
 
-const AuthContextProvider = ({children}) => {
-    const createUser = async(email,password) => {
-        try {
-            let userCredential= await createUserWithEmailAndPassword(auth, email, password)
-            console.log(userCredential)
-        } catch (error) {
-            console.log(error.message)    
-        }
-    };
-    const signIn = async(email,password) => {
-        try {
-            let userCredential=await signInWithEmailAndPassword(auth, email, password)
-            console.log(userCredential)
-        } catch (error) {
-            console.log(error.message)    
-        }
-    };
-    const values={createUser,signIn}
-  return (
-    <AuthContext.Provider 
-    value={values}     
-    >
-        {children}
-      
-    </AuthContext.Provider>
-  )
-}
+const AuthContextProvider = ({ children }) => {
+    let navigate=useNavigate()
+	const createUser = async (email, password) => {
+		try {
+			let userCredential = await createUserWithEmailAndPassword(
+				auth,
+				email,
+				password
+			);
+			console.log(userCredential);
+            navigate('/')
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+	const signIn = async (email, password) => {
+		try {
+			let userCredential = await signInWithEmailAndPassword(
+				auth,
+				email,
+				password
+			);
+			console.log(userCredential);
+            navigate('/')
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+	const values = { createUser, signIn };
+	return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
+};
 
-export default AuthContextProvider
+export default AuthContextProvider;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes,Route } from "react-router-dom";
+import {  Routes,Route } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
@@ -7,7 +7,7 @@ import Main from "../pages/Main";
 import MovieDetail from "../pages/MovieDetail";
 
 const AppRouter = () => {
-  return <BrowserRouter>
+  return <>
   <Navbar/>
   <Routes>
     <Route  path="/" element={<Main />}  />
@@ -15,7 +15,7 @@ const AppRouter = () => {
     <Route  path="/register" element={<Register />}  />
     <Route  path="/details/:id" element={<MovieDetail />}  />
   </Routes>
-    </BrowserRouter>;
+    </>;
 };
 
 export default AppRouter;

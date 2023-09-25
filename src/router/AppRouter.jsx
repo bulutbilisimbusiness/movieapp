9234ed404d0e0014275e23461e316f9382b3928f
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2023-09-25 12:09:35 +0300

    route

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes,Route } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import Main from "../pages/Main";
+import MovieDetail from "../pages/MovieDetail";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return <BrowserRouter>
+  <Navbar/>
+  <Routes>
+    <Route  path="/" element={<Main />}  />
+    <Route  path="/login" element={<Login />}  />
+    <Route  path="/register" element={<Register />}  />
+    <Route  path="/details/:id" element={<MovieDetail />}  />
+  </Routes>
+    </BrowserRouter>;
 };
 
 export default AppRouter;

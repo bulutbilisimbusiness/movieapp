b376bdf274458d69312554b5d413a87b83496cbb
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2023-09-25 17:52:25 +0300

    signin

---
+++ src/context/AuthContext.js
@@ -0,0 +1,39 @@
+import React, { createContext } from 'react'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from '../auth/firebase';
+export const AuthContext=createContext()
+
+//* with custom hook
+/* export const useAuthContext = () =>{
+    return useContext(AuthContext)
+} */
+
+const AuthContextProvider = ({children}) => {
+    const createUser = async(email,password) => {
+        try {
+            let userCredential= await createUserWithEmailAndPassword(auth, email, password)
+            console.log(userCredential)
+        } catch (error) {
+            console.log(error.message)    
+        }
+    };
+    const signIn = async(email,password) => {
+        try {
+            await signInWithEmailAndPassword(auth, email, password)
+            
+        } catch (error) {
+            console.log(error.message)    
+        }
+    };
+    const values={createUser,signIn}
+  return (
+    <AuthContext.Provider 
+    value={values}     
+    >
+        {children}
+      
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthContextProvider

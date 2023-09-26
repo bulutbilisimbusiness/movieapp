e283435d21bd94e5706fe4145a4d74d88db2ab2a
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2023-09-26 16:47:05 +0300

    total

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,21 +1,26 @@
 import React from "react";
-import {  Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
 import Main from "../pages/Main";
 import MovieDetail from "../pages/MovieDetail";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
-  return <>
-  <Navbar/>
-  <Routes>
-    <Route  path="/" element={<Main />}  />
-    <Route  path="/login" element={<Login />}  />
-    <Route  path="/register" element={<Register />}  />
-    <Route  path="/details/:id" element={<MovieDetail />}  />
-  </Routes>
-    </>;
+	return (
+		<>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Main />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route element={<PrivateRouter />}>
+					<Route path="/details/:id" element={<MovieDetail />} />
+				</Route>
+			</Routes>
+		</>
+	);
 };
 
 export default AppRouter;
---
+++ src/router/PrivateRouter.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
+import { Navigate, Outlet } from 'react-router'
+
+const PrivateRouter = () => {
+    const {currentUser} = useContext(AuthContext)
+  return (
+    currentUser ? <Outlet/> : <Navigate to="/login" replace/>
+  )
+}
+
+export default PrivateRouter
